1f5adfff61cae393e51c51ca57793d258ee6d33b
“VenuReddy773” <venu706@gmail.com>
2021-11-11 21:30:11 +0530

    [VenuGopalReddy] Added: Swapping Of two Numbers Program

---
+++ BasicCorePrograms/BasicCorePrograms/SwapTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class SwapTwoNumbers
+    {
+        public void Swapping()
+        {
+            int firstNumber, secondNumber;
+            Console.WriteLine("Enter First Number");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            Console.WriteLine("After Swapping First number="+firstNumber+" and Second Number="+secondNumber+"\n");
+        }
+    }
+}
