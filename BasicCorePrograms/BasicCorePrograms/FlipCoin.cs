e275d511f18b17a5d8f3f0208a384f95bf5abe55
“VenuReddy773” <venu706@gmail.com>
2021-11-11 11:34:46 +0530

    [VenuGopalReddy] Added Flip a Coin Program

---
+++ BasicCorePrograms/BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class FlipCoin
+    {
+        public void Play()
+        {
+            Console.WriteLine("Enter No. of Times To Flip Coin");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int headCount = 0, tailCount = 0;
+            for(int i=0; i < number; i++)
+            {
+                double flip = random.NextDouble();
+                if(flip < 0.5)
+                {
+                    Console.WriteLine("Heads");
+                    headCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tailCount++;
+                }
+                
+            }
+            headCount *= (100 / number);
+            tailCount *= (100 / number);
+            Console.WriteLine("Headscount percentage is:"+headCount+"%\n"+"Tailscount percentage is:"+tailCount+"%\n");                           
+        }
+    }
+}
---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin coin = new FlipCoin();
+                        coin.Play();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+                    
+            }
+        }
+    }
+}
