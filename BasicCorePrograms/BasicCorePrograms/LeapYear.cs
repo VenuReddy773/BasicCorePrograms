c87b34ac4d14d4b6d5af16e4e7140dc3b606df55
“VenuReddy773” <venu706@gmail.com>
2021-11-11 12:33:14 +0530

    [VenuGopalReddy] Added Leap Year Check Program

---
+++ BasicCorePrograms/BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class LeapYear
+    {
+        public void LeapYearCheck()
+        {
+            Console.WriteLine("Enter the Year To Check Whether Leap Year or not");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if( year % 4 == 0 && year % 100 != 0 || year % 400 ==0)
+            {
+                Console.WriteLine(year +" is Leap year\n");
+            }
+            else
+            {
+                Console.WriteLine(year +" is not a leap year\n");
+            }               
+        }
+    }
+}
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -8,7 +8,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.Exit");
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.flipcoin\n 2.LeapYearCheck\n 3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -17,7 +17,14 @@ namespace BasicCorePrograms
                         coin.Play();
                         break;
                     case 2:
+                        LeapYear check = new LeapYear();
+                        check.LeapYearCheck();
+                        break;
+                    case 3:
                         flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Correct Option..\n");
                         break;
                 }
                     
