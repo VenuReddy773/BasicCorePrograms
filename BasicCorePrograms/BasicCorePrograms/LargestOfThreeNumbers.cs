46106bbf5799c742ea7d8306d9d6c9a748247927
“VenuReddy773” <venu706@gmail.com>
2021-11-12 11:35:40 +0530

    [VenuGopalReddy] Added: Largest Of Three Numbers Program

---
+++ BasicCorePrograms/BasicCorePrograms/LargestOfThreeNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class LargestOfThreeNumbers
+    {
+        public void LargestOfThree()
+        {
+            int first, second, third;
+            Console.WriteLine("Enter First Number");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            second= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Third Number");
+            third = Convert.ToInt32(Console.ReadLine());
+            if(first > second && first > third )
+            {
+                Console.WriteLine(first + " is the Greatest Among All Three");
+            }
+            if(second > first && second > third )
+            {
+                Console.WriteLine(second + " is the Greatest Among All Three");
+            }
+            else
+            {
+                Console.WriteLine(third + " is the Greatest Among All Three");
+            }
+        }
+    }
+}
